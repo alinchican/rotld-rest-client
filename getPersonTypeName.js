dabb09055d87703c86acdc8cf492a36161671196
Alin-Andrei Chican <alin@chican.ro>
2018-05-11 02:17:11 +0300

    Refactor getPersonTypeName

--- getPersonTypeName.js
+++ getPersonTypeName.js
@@ -1,24 +1,8 @@
-const personTypes = {
-  p: "person",
-  ap: "authorized person",
-  j: "juridical person",
-  nc: "non-commercial",
-  c: "commercial",
-  gi: "government institution",
-  pi: "public institution",
-  o: "other"
-};
+const personTypes = require("./personTypes");
+const isValidPersonType = require("./isValidPersonType");
 
 const getPersonTypeName = personType => {
-  if (typeof personType !== "string") {
-    throw new TypeError("Argument must be a string.");
-  }
-
-  if (!personType) {
-    throw new TypeError("Can't decode an empty person type.");
-  }
-
-  if (!Object.keys(personTypes).includes(personType)) {
+  if (!isValidPersonType(personType)) {
     throw new Error(
       `Person type is invalid. Must be one of ${Object.keys(personTypes).join(
         ", "
