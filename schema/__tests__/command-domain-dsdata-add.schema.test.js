b0a4766b25faafa95c588fa866dc70012a4a706a
Alin-Andrei Chican <alin@chican.ro>
2018-06-01 22:31:50 +0300

    Improve schema test

--- schema/__tests__/command-domain-dsdata-add.schema.test.js
+++ schema/__tests__/command-domain-dsdata-add.schema.test.js
@@ -8,3 +8,274 @@ test("should throw without argument", () => {
 test("should throw on empty argument", () => {
   expect(() => schemaValidator.assert({}, schema)).toThrow();
 });
+
+test("should throw on invalid argument", () => {
+  expect(() => schemaValidator.assert([], schema)).toThrow();
+});
+
+test("should throw with invalid domain variable type", () => {
+  expect(() =>
+    schemaValidator.assert(
+      {
+        domain: ["xn--yla.ro"],
+        keytag: 65534,
+        alg: 3,
+        digest_type: 1,
+        digest: "1111111111111111111111111111111111111111"
+      },
+      schema
+    )
+  ).toThrow();
+});
+
+test("should throw without domain", () => {
+  expect(() =>
+    schemaValidator.assert(
+      {
+        keytag: 65534,
+        alg: 3,
+        digest_type: 1,
+        digest: "1111111111111111111111111111111111111111"
+      },
+      schema
+    )
+  ).toThrow();
+});
+
+test("should throw with invalid domain (punycode)", () => {
+  expect(() =>
+    schemaValidator.assert(
+      {
+        domain: "ș.ro",
+        keytag: 65534,
+        alg: 3,
+        digest_type: 1,
+        digest: "1111111111111111111111111111111111111111"
+      },
+      schema
+    )
+  ).toThrow();
+});
+
+test("should throw with invalid keytag variable type", () => {
+  expect(() =>
+    schemaValidator.assert(
+      {
+        domain: "xn--yla.ro",
+        keytag: ["65534"],
+        alg: 3,
+        digest_type: 1,
+        digest: "1111111111111111111111111111111111111111"
+      },
+      schema
+    )
+  ).toThrow();
+});
+
+test("should throw without keytag", () => {
+  expect(() =>
+    schemaValidator.assert(
+      {
+        domain: "xn--yla.ro",
+        alg: 3,
+        digest_type: 1,
+        digest: "1111111111111111111111111111111111111111"
+      },
+      schema
+    )
+  ).toThrow();
+});
+
+test("should throw with invalid keytag (min)", () => {
+  expect(() =>
+    schemaValidator.assert(
+      {
+        domain: "xn--yla.ro",
+        keytag: "-66",
+        alg: 3,
+        digest_type: 1,
+        digest: "1111111111111111111111111111111111111111"
+      },
+      schema
+    )
+  ).toThrow();
+});
+
+test("should throw with invalid keytag (max)", () => {
+  expect(() =>
+    schemaValidator.assert(
+      {
+        domain: "xn--yla.ro",
+        keytag: "66536",
+        alg: 3,
+        digest_type: 1,
+        digest: "1111111111111111111111111111111111111111"
+      },
+      schema
+    )
+  ).toThrow();
+});
+
+test("should throw with invalid alg variable type", () => {
+  expect(() =>
+    schemaValidator.assert(
+      {
+        domain: "xn--yla.ro",
+        keytag: "65534",
+        alg: [16],
+        digest_type: 1,
+        digest: "1111111111111111111111111111111111111111"
+      },
+      schema
+    )
+  ).toThrow();
+});
+
+test("should throw without alg", () => {
+  expect(() =>
+    schemaValidator.assert(
+      {
+        domain: "xn--yla.ro",
+        keytag: "65534",
+        digest_type: 1,
+        digest: "1111111111111111111111111111111111111111"
+      },
+      schema
+    )
+  ).toThrow();
+});
+
+test("should throw with invalid alg", () => {
+  expect(() =>
+    schemaValidator.assert(
+      {
+        domain: "xn--yla.ro",
+        keytag: "66534",
+        alg: 16,
+        digest_type: 1,
+        digest: "1111111111111111111111111111111111111111"
+      },
+      schema
+    )
+  ).toThrow();
+});
+
+test("should throw with invalid digest type variable type", () => {
+  expect(() =>
+    schemaValidator.assert(
+      {
+        domain: "xn--yla.ro",
+        keytag: "66534",
+        alg: 16,
+        digest_type: [1],
+        digest: "1111111111111111111111111111111111111111"
+      },
+      schema
+    )
+  ).toThrow();
+});
+
+test("should throw without digest type", () => {
+  expect(() =>
+    schemaValidator.assert(
+      {
+        domain: "xn--yla.ro",
+        keytag: "66534",
+        alg: 16,
+        digest: "1111111111111111111111111111111111111111"
+      },
+      schema
+    )
+  ).toThrow();
+});
+
+test("should throw with invalid digest type", () => {
+  expect(() =>
+    schemaValidator.assert(
+      {
+        domain: "xn--yla.ro",
+        keytag: "66534",
+        alg: 16,
+        digest_type: 3,
+        digest: "1111111111111111111111111111111111111111"
+      },
+      schema
+    )
+  ).toThrow();
+});
+
+test("should throw with invalid digest length (max, digest_type 1)", () => {
+  expect(() =>
+    schemaValidator.assert(
+      {
+        domain: "xn--yla.ro",
+        keytag: "66534",
+        alg: 16,
+        digest_type: 3,
+        digest: "11111111111111111111111111111111111111111"
+      },
+      schema
+    )
+  ).toThrow();
+});
+
+test("should throw with invalid digest length (min, digest_type 1)", () => {
+  expect(() =>
+    schemaValidator.assert(
+      {
+        domain: "xn--yla.ro",
+        keytag: "66534",
+        alg: 16,
+        digest_type: 3,
+        digest: "1111111111111111111111111111111111111"
+      },
+      schema
+    )
+  ).toThrow();
+});
+
+test("should throw with invalid digest length (max, digest_type 2)", () => {
+  expect(() =>
+    schemaValidator.assert(
+      {
+        domain: "xn--yla.ro",
+        keytag: "66534",
+        alg: 16,
+        digest_type: 3,
+        digest:
+          "11111111111111111111111111111111111111111111111111111111111111111111111111111111"
+      },
+      schema
+    )
+  ).toThrow();
+});
+
+test("should throw with invalid digest length (min, digest_type 2)", () => {
+  expect(() =>
+    schemaValidator.assert(
+      {
+        domain: "xn--yla.ro",
+        keytag: "66534",
+        alg: 16,
+        digest_type: 3,
+        digest: "1111111111111111111111111111111111111"
+      },
+      schema
+    )
+  ).toThrow();
+});
+
+test("should return undefined", () => {
+  expect(
+    schemaValidator.assert(
+      {
+        domain: "xn--yla.ro",
+        keytag: 65534,
+        alg: 3,
+        digest_type: 1,
+        digest: "  1111111111111111111111111111111111111111"
+      },
+      schema
+    )
+  ).toBeUndefined();
+});
